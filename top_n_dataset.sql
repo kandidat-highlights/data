b8ad88f022f8df16901173b86b59b0c8913e03d7
Maxim Goretskyy <13maxim13@live.se>
2017-02-18 19:55:05 +0100

    Implements support for choosing top n users, also pushes file with partitions of files

---
+++ top_n_dataset.sql
@@ -0,0 +1,37 @@
+CREATE TABLE data (
+    username text,
+    vote integer,
+    subreddit text DEFAULT '',
+    title text DEFAULT '',
+    content text
+);
+-- change path to the file.
+\copy data from '~/Desktop/original_data_9feb.csv' with NULL AS ' ' csv;
+
+CREATE TABLE processeddata (
+	id SERIAL NOT NULL,--This will be used to split training/valid data.select
+	title text,
+	subreddit text,
+	content text,
+	vote integer,
+	users text
+);
+
+CREATE TABLE allusers(
+	users text
+);
+-- Change the number after "desc limit" in order to choose how many users you want
+INSERT INTO processeddata (title, subreddit, content, vote, users) SELECT regexp_replace(title, E'[\\n\\r|\\t]+', ' ', 'g' ) AS title, subreddit, content, vote, string_agg(username, ', ') AS users
+FROM data WHERE title <> '' AND title IS NOT NULL AND username in (select username from data group by username order by count(username) desc limit 5) 
+GROUP BY title, subreddit, content, vote ORDER BY random();
+
+/*
+	We used random order before, everything is fine :)
+	70%ish training data 
+	20%ish validation data
+	10%ish test data
+
+*/
+\copy (select regexp_replace(title, '^\s+', '') AS title, users FROM processeddata WHERE vote=1 AND ID < floor((select max(id) from processeddata) * 0.7)) to '~/Desktop/training_data_top_n.csv' With CSV;
+\copy (select regexp_replace(title, '^\s+', '') AS title, users FROM processeddata WHERE vote=1 AND ID > floor((select max(id) from processeddata) * 0.7)+1 AND ID < floor((select max(id) from processeddata) * 0.9)) to '~/Desktop/validation_data_top_n.csv' With CSV;
+\copy (select regexp_replace(title, '^\s+', '') AS title, users FROM processeddata WHERE vote=1 AND ID > floor((select max(id) from processeddata) * 0.9) +1) to '~/Desktop/testing_data_top_n.csv' With CSV;
